9c666defeadc35300b07ab1609fdb9e43eaf88ae
Gitlab CI <ruben.malchow@gmail.com>
2021-03-01 08:40:57 +0100

    added client ip

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,7 @@ set -e
 set +x
 export TAG_A=harbor.rand0m.me/public/${CI_PROJECT_NAME}:latest
 export TAG_B=harbor.rand0m.me/public/${CI_PROJECT_NAME}:RELEASE
+export TAG_C=rmalchow/${CI_PROJECT_NAME}:RELEASE
 
 docker login -u ${CI_EMAIL} -p ${CI_PASSWORD} harbor.rand0m.me
 docker pull ${TAG_A}
@@ -10,6 +11,5 @@ docker tag ${TAG_A} ${TAG_B}
 docker push ${TAG_B}
 
 docker login -u ${DH_USER} -p ${DH_PW}
-docker pull ${TAG_A}
-docker tag ${TAG_A} ${TAG_B}
-docker push ${TAG_B}
+docker tag ${TAG_A} ${TAG_C}
+docker push ${TAG_C}
