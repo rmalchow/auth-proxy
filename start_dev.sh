00651a0697d29478dd782c04f31cf18207298525
Gitlab CI <ruben.malchow@gmail.com>
2021-03-13 18:53:18 +0100

    change build image

--- start_dev.sh
+++ start_dev.sh
@@ -8,8 +8,8 @@ if [ -z ${port} ]; then
 fi
 
 #ln -s ./vrtlr-app/config/application.yml . || true
-docker run --name vrtlr-build -ti -v $(pwd):/app --entrypoint /bin/bash harbor.cloudpowered.services/mcon-group-public/centos8-base:latest -c "cd /app; mvn -s ./settings.dev.xml  -T 16 clean package"
-docker run --name vrtlr-run -d -e host=${url} -v $(pwd):/app -p ${port}:8080 --entrypoint /bin/bash harbor.cloudpowered.services/mcon-group-public/centos8-base:latest -c "cd /app; bash scripts/dev.sh"
+docker run --name vrtlr-build -ti -v $(pwd):/app --entrypoint /bin/bash harbor.rand0m.me/public/centos8-base:latest -c "cd /app; mvn -s ./settings.dev.xml  -T 16 clean package"
+docker run --name vrtlr-run -d -e host=${url} -v $(pwd):/app -p ${port}:8080 --entrypoint /bin/bash harbor.rand0m.me/public/centos8-base:latest -c "cd /app; bash scripts/dev.sh"
 
 
 docker logs -f vrtlr-run
