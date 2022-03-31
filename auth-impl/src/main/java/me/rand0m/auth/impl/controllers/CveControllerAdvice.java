1520cce4c1757aabf8836e8c7a1bba4ed6e91463
Ruben Malchow <ruben.malchow@gmail.com>
2022-03-31 20:29:33 +0200

    cve

---
+++ auth-impl/src/main/java/me/rand0m/auth/impl/controllers/CveControllerAdvice.java
@@ -0,0 +1,17 @@
+package me.rand0m.auth.impl.controllers;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+@Order(10000)
+public class CveControllerAdvice {
+	
+	@InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        String[] denylist = new String[]{"class.*", "Class.*", "*.class.*", "*.Class.*"};
+        dataBinder.setDisallowedFields(denylist);
+    }
+}
